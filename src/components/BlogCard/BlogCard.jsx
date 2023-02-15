ed23451945131ff28dded0148583e43dfdd24fcc
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-02-15 14:42:24 +0200

    colorized 1st card

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+import css from './BlogCard.module.css'
+import {formatDistanceToNow} from 'date-fns'
 
 export const BlogCard = ({
   poster,
@@ -10,26 +12,26 @@ export const BlogCard = ({
   postedAt,
 }) => {
   return (
-    <div>
-      <img src={poster} alt={tag} />
-      <span>{tag}</span>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <div>
-        <img src={avatar} alt="" />
-        <p>{name}</p>
-        <p>{postedAt}</p>
+    <div className={css.section}>
+      <img className={css.image}src={poster} alt={tag} />
+      <span className={css.box}>{tag}</span>
+      <h2 className={css.title}>{title}</h2>
+      <p className={css.text}>{description}</p>
+      <div className={css.loverBox}>
+        <img className={css.additionalImage}src={avatar} alt="" />
+        <p className={css.loverText}>{name}</p>
+        <p className={css.date}>{ formatDistanceToNow(new Date(postedAt),{ addSuffix: true })}</p>
       </div>
     </div>
   );
 };
 
 BlogCard.propTypes = {
-  poster:PropTypes.string.isRequired,
-  tag:PropTypes.string.isRequired,
-  title:PropTypes.string.isRequired,
-  description:PropTypes.string.isRequired,
-  name:PropTypes.string.isRequired,
-  avatar:PropTypes.string.isRequired,
-  postedAt:PropTypes.string.isRequired,
-}
+  poster: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  postedAt: PropTypes.string.isRequired,
+};
